e2676d558232a4c45132bcf8eeb89d1733a44392
Karaulovskin <karaulovskin@yandex.ru>
2020-09-17 12:39:07 +0300

    clean

--- local/templates/main/frontend/app/app.js
+++ local/templates/main/frontend/app/app.js
@@ -24,34 +24,14 @@ import Utils from'./js/utils/utils'
 import SvgUse from'./js/svgUse'
 import Forms from'./js/forms/forms'
 import Modals from'./js/modals'
-import Map from'./js/map'
 import MapObject from'./js/map-object'
-import Video from'./js/video'
 import Sliders from'./js/sliders'
-import PageSliderScrollMagic from'./js/page-slider-scroll-magic'
-import PageSlider from'./js/page-slider'
-import Anchor from'./js/anchor'
-import Header from'./js/header'
-import Scrollbar from'./js/scrollbar'
-import CityList from'./js/scroller-horizontal'
-import Animate from'./js/animate'
-import Location from'./js/location'
 import Search from'./js/search'
-import Player from'./js/player'
-import PlaceholderStar from './js/placeholder-star'
-import Basket from './js/basket'
-import PictureHover from './js/picture-hover'
-import BxFilter from './js/bx-filter'
-import Sorting from './js/sorting'
-import Tabs from './js/tabs'
-import CardAudio from './js/card-audio'
-import Parallaxxx from './js/parallax'
-import MobileMenu from './js/mobile-menu'
-import SlideUp from './js/slide-up'
-import mobileSlider from './js/mobile-slider'
-import Media from './js/media'
-import NavAlphabet from './js/nav-alphabet'
-import Placeholder from './js/placeholder'
+// import MapObject from'./js/map'
+// import MapObject from'./js/scrollbar'
+// import Video from'./js/video'
+// import Tabs from './js/tabs'
+// import Media from './js/media'
 
 // Load styles
 import './styles/app.js';
@@ -78,44 +58,20 @@ document.addEventListener('DOMContentLoaded', function() {
     if('ontouchstart' in window || navigator.maxTouchPoints) $(document.body).addClass("touch");
 
     App.Utils = new Utils();
-    App.Forms = new Forms();
     App.SvgUse = new SvgUse();
+    App.Forms = new Forms();
     App.Modals = new Modals();
-    // App.Map = new Map();
     App.MapObject = new MapObject();
-    App.Video = new Video();
     App.Sliders = new Sliders();
-    // App.PageSlider = new PageSlider();
-    App.PageSliderScrollMagic = new PageSliderScrollMagic();
-    // App.Anchor = new Anchor();
-    App.Header = new Header();
-    // App.Scrollbar = new Scrollbar();
-    App.CityList = new CityList();
-    // App.Animate = new Animate();
-    App.Location = new Location();
     App.Search = new Search();
-    App.Player = new Player();
-    // App.PlaceholderStar = new PlaceholderStar();
-    App.Basket = new Basket();
-    App.PictureHover = new PictureHover();
-    App.BxFilter = new BxFilter();
-    App.Sorting = new Sorting();
-    App.Tabs = new Tabs();
-    App.CardAudio = new CardAudio();
-    // App.Parallaxxx = new Parallaxxx();
-    App.MobileMenu = new MobileMenu();
-    App.SlideUp = new SlideUp();
-    App.mobileSlider = new mobileSlider();
-    App.Media = new Media();
-    App.NavAlphabet = new NavAlphabet();
-    App.Placeholder = new Placeholder();
+    // App.Map = new Map();
+    // App.Video = new Video();
+    // App.Scrollbar = new Scrollbar();
+    // App.Tabs = new Tabs();
+    // App.Media = new Media();
 
     $('[data-inputmask]').each(function () {
         IMask($(this)[0], {mask: "0 (000) 000-0000"});
-    });
-
-    $('[data-inputmask-inn]').each(function () {
-        IMask($(this)[0], {mask: "0000000000"});
     });
 
     // prevent copying
--- local/templates/main/frontend/app/styles/app.js
+++ local/templates/main/frontend/app/styles/app.js
@@ -3,28 +3,18 @@ import 'normalize.css';
 import 'animate.css'
 
 require("./variables.css");
-require("./media.css");
-require("./helpers.css");
-require("./grid.css");
-require("./icons.css");
 require("./styles.css");
+require("./bx-filter.css");
+require("./editor.css");
+require("./fancybox.css");
 require("./fonts.css");
 require("./forms.css");
-require("./fancybox.css");
+require("./grid.css");
+require("./helpers.css");
+require("./icons.css");
 require("./lazy-load.css");
-require("./swiper.css");
-require("./icon-info-list.css");
+require("./media.css");
 require("./search.css");
-require("./price.css");
-require("./placeholder-star.css");
-require("./bx-filter.css");
-require("./picture-hover.css");
-require("./editor.css");
-require("./scrollbar.css");
-require("./bx-search-order.css");
-require("./bx-sbb-empty-cart.css");
-require("./page-slider-scroll-magic.css");
-require("./ymaps.css");
 
 /*All components*/
 
